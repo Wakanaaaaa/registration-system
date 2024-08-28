4671996da297fa63abef3315838cbf612414297a
YukaChoco <sugar.ore.yk@gmail.com>
2024-08-28 17:38:30 +0900

    入力欄をfirebaseに反映させた

--- app/input-episodes/[testerNumber]/page.tsx
+++ app/input-episodes/[testerNumber]/page.tsx
@@ -1,12 +1,32 @@
 "use client";
-import { doc, getDoc } from "firebase/firestore";
+import { getDoc, doc, addDoc, collection } from "firebase/firestore";
 import { db } from "@/app/firebase";
 import styles from "./page.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+
+interface Episode {
+  when: string;
+  where: string;
+  who: string;
+  what: string;
+}
 
 export default function Home({ params }: { params: { testerNumber: string } }) {
   const router = useRouter();
+  const { testerNumber } = params;
+  const [episodeData, setEpisodeData] = useState<Episode[] | null>(null);
+  const [inputData, setInputData] = useState<Episode>({
+    when: "",
+    where: "",
+    who: "",
+    what: "",
+  });
+  const [episodeCount, setEpisodeCount] = useState(0);
+
+  console.log(episodeData);
+  console.log(inputData);
+
   useEffect(() => {
     const fetchData = async () => {
       const docSnap = await getDoc(doc(db, "4Wwords", "1", "episodes", "1"));
@@ -15,9 +35,24 @@ export default function Home({ params }: { params: { testerNumber: string } }) {
     fetchData();
   }, []);
 
+  const onChange = (e: any) => {
+    setInputData({ ...inputData, [e.target.id]: e.target.value });
+  };
+
   const onSubmit = (e: any) => {
     e.preventDefault();
-    console.log("submit");
+    console.log(inputData);
+    // firebaseにデータを登録
+    addDoc(collection(db, "4Wwords", testerNumber, "episodes"), inputData);
+    setEpisodeCount(episodeCount + 1);
+    setEpisodeData((prev) => (prev ? [...prev, inputData] : [inputData]));
+    setInputData({
+      when: "",
+      where: "",
+      who: "",
+      what: "",
+    });
+
     // router.push("/input-episodes");
   };
 
@@ -29,29 +64,66 @@ export default function Home({ params }: { params: { testerNumber: string } }) {
       {/* エピソードを入力 */}
       <form action="post" onSubmit={onSubmit} className={styles.form}>
         <div className={styles.input}>
-          <label htmlFor="when" className={styles.label}>いつ</label>
-          <input id="when" type="text" />
+          <label htmlFor="when" className={styles.label}>
+            いつ
+          </label>
+          <input
+            id="when"
+            type="text"
+            value={inputData.when}
+            onChange={onChange}
+          />
         </div>
 
         <div className={styles.input}>
           <label htmlFor="where">どこで</label>
-          <input id="where" type="text" />
+          <input
+            id="where"
+            type="text"
+            value={inputData.where}
+            onChange={onChange}
+          />
         </div>
 
         <div className={styles.input}>
-          <label htmlFor="who" className={styles.label}>誰と</label>
-          <input id="who" type="text" />
+          <label htmlFor="who" className={styles.label}>
+            誰と
+          </label>
+          <input
+            id="who"
+            type="text"
+            value={inputData.who}
+            onChange={onChange}
+          />
         </div>
 
         <div className={styles.input}>
-          <label htmlFor="what" className={styles.label}>何を</label>
-          <input id="what" type="text" />
+          <label htmlFor="what" className={styles.label}>
+            何を
+          </label>
+          <input
+            id="what"
+            type="text"
+            value={inputData.what}
+            onChange={onChange}
+          />
         </div>
 
         <br />
 
         <button type="submit">登録</button>
       </form>
+
+      {episodeData &&
+        episodeData.map((data, index) => (
+          <div key={index}>
+            <h2>エピソード{index + 1}</h2>
+            <p>{data.when}</p>
+            <p>{data.where}</p>
+            <p>{data.who}</p>
+            <p>{data.what}</p>
+          </div>
+        ))}
     </main>
   );
 }
